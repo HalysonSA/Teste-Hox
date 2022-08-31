ed6c719508b32f1a8e3ad4f6038694ebe6a1d672
Halyson <92060880+HalysonSA@users.noreply.github.com>
2022-08-30 23:18:12 -0300

    +hook form

--- src/pages/login.js
+++ src/pages/login.js
@@ -19,17 +19,18 @@ import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
 import { useState } from 'react';
 
+import { useForm } from 'react-hook-form';
+
 import checkUser from '../authentication';
 
 function Login() {
+    const { register, handleSubmit } = useForm();
+
     const [show, setShow] = useState(false);
     const handleClick = () => setShow(!show);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    async function handleSubmit() {
-        const response = await checkUser(email, password);
+    async function handleChange(data) {
+        const response = await checkUser(data.email, data.password);
         if (response !== null) {
             localStorage.setItem('token', response);
             window.location.href = '/';
@@ -42,57 +43,63 @@ function Login() {
         <Flex justify="center" bg="black" align="center" h="100vh">
             <Center bg="white" h="70vh" px="2em" borderRadius={'2xl'}>
                 <ToastContainer />
-                <FormControl>
-                    <Center>
-                        <Image src={Hox} w="70px" h="70px" alt="hox" />
-                    </Center>
-                    <Center mb="3em">
-                        <Text fontSize="3xl" fontWeight={'bold'}>
-                            Bem Vindo a Hox
-                        </Text>
-                    </Center>
-                    <FormLabel> Email</FormLabel>
-                    <Input
-                        isRequired
-                        type="email"
-                        _hover={{ borderColor: 'black' }}
-                        focusBorderColor="black"
-                        placeholder="Digite seu email"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <FormLabel> Senha </FormLabel>
-
-                    <InputGroup>
+                <form onSubmit={handleSubmit(handleChange)}>
+                    <FormControl>
+                        <Center>
+                            <Image src={Hox} w="70px" h="70px" alt="hox" />
+                        </Center>
+                        <Center mb="3em">
+                            <Text fontSize="3xl" fontWeight={'bold'}>
+                                Bem Vindo a Hox
+                            </Text>
+                        </Center>
+                        <FormLabel> Email</FormLabel>
                         <Input
+                            isRequired
+                            type="email"
+                            {...register('email')}
                             _hover={{ borderColor: 'black' }}
                             focusBorderColor="black"
-                            pr="4.5rem"
-                            type={show ? 'text' : 'password'}
-                            placeholder="********"
-                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Digite seu email"
                         />
-                        <InputRightElement width="4.5rem">
-                            <Button h="1.75rem" size="sm" onClick={handleClick}>
-                                {show ? <ViewOffIcon /> : <ViewIcon />}
-                            </Button>
-                        </InputRightElement>
-                    </InputGroup>
-                    <Button
-                        bg="black"
-                        w="100%"
-                        mt="3"
-                        _focus={{ bg: 'black' }}
-                        _hover={{
-                            boxShadow:
-                                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)',
-                        }}
-                        onClick={handleSubmit}
-                    >
-                        <FormLabel m="0" color="white" fontWeight={'bold'}>
-                            Login
-                        </FormLabel>
-                    </Button>
-                </FormControl>
+                        <FormLabel> Senha </FormLabel>
+
+                        <InputGroup>
+                            <Input
+                                _hover={{ borderColor: 'black' }}
+                                focusBorderColor="black"
+                                pr="4.5rem"
+                                type={show ? 'text' : 'password'}
+                                {...register('password')}
+                                placeholder="********"
+                            />
+                            <InputRightElement width="4.5rem">
+                                <Button
+                                    h="1.75rem"
+                                    size="sm"
+                                    onClick={handleClick}
+                                >
+                                    {show ? <ViewOffIcon /> : <ViewIcon />}
+                                </Button>
+                            </InputRightElement>
+                        </InputGroup>
+                        <Button
+                            bg="black"
+                            w="100%"
+                            mt="3"
+                            _focus={{ bg: 'black' }}
+                            _hover={{
+                                boxShadow:
+                                    '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)',
+                            }}
+                            type="submit"
+                        >
+                            <FormLabel m="0" color="white" fontWeight={'bold'}>
+                                Login
+                            </FormLabel>
+                        </Button>
+                    </FormControl>
+                </form>
             </Center>
         </Flex>
     );
